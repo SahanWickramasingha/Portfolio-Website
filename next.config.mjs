7930bc37396ea275fd04b94a1cf4b28805de1d4f
Sahan  Wickramasingha <Sahanwikrama29@gmail.com>
2026-02-01 13:16:03 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
-const repoName = "Portfolio-Website";
-
 const nextConfig = {
-  output: "export",
-  distDir: "docs",
-  basePath: "/Portfolio-Website",
-  assetPrefix: "/Portfolio-Website/",
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
