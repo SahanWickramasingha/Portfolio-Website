dc0c1ac2019e2d413f399362abb12ee5c8d0afc4
HemangaChamod <hemangachamod@gmail.com>
2026-02-01 12:40:41 +0530

    first commit

---
+++ app/layout.js
@@ -0,0 +1,28 @@
+import { Outfit, Ovo } from "next/font/google";
+import "./globals.css";
+
+const outfit = Outfit({
+    subsets: ["latin"],
+    weight: ["400", "500", "600"],
+    variable: "--font-outfit",
+});
+const ovo = Ovo({
+    subsets: ["latin"],
+    weight: ["400"],
+    variable: "--font-ovo",
+});
+
+export const metadata = {
+    title: "Sahan Wickramasingha - PrebuiltMobile,Desktop,Web Development",
+    description: "I build intelligent and scalable mobile, desktop, and web applications by leveraging modern frameworks and machine learning tools.",
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            <body className="font-Outfit leading-8 dark:bg-darkTheme dark:text-white">
+                {children}
+            </body>
+        </html>
+    );
+}
